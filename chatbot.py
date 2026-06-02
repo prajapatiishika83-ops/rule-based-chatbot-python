732202d053d2a15c5609722aaf2b6865e7a4ca0a
prajapatiishika83-ops <prajapatiishika83@gmail.com>
2026-06-02 16:06:38 +0530

    Implement basic chatbot interaction loop
    A simple, lightweight, and interactive Rule-Based Chatbot built using Python. This project utilizes standard conditional mapping (if-elif-else logic) and string normalization to handle real-time user inputs, basic greetings, and predefined FAQs. Perfect for beginners learning Python loop control and basic text automation.

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+while True: 
+    user = input("You: ").lower() 
+    if user == "hello" or user == "hi": 
+        print("Bot: Hello! Nice to meet you.") 
+    elif user == "how are you": 
+        print("Bot: I am fine. Thank you!") 
+    elif user == "what is your name": 
+        print("Bot: My name is AI Chatbot.") 
+    elif user == "bye" or user == "exit": 
+        print("Bot: Goodbye!") 
+        break 
+     else: 
+        print("Bot: Sorry, I don't understand.") 
